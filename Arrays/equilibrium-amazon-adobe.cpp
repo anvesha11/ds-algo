0129afdb899ed7965c2612c6ab4ffeb774e835b4
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-06-11 14:27:54 +0530

    Add files via upload

---
+++ Arrays/equilibrium-amazon-adobe.cpp
@@ -0,0 +1,19 @@
+int equilibriumPoint(long long a[], int n) {
+
+    // Your code here
+    long long i, sum_left=0,sum_right=0;
+    if(n==1)
+    return 1;
+    for(i=1;i<n;i++)
+    sum_right+=a[i];
+    //sum_right-=a[1];
+    for(i=1;i<n;i++)
+    {
+        sum_left+=a[i-1];
+        sum_right-=a[i];
+        if(sum_left == sum_right)
+            return i+1;
+    }
+    if(sum_right!=sum_left)
+    return -1;
+}
