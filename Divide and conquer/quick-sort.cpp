4ec05d6d60db562883cb76259555e710e4fac07c
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-06-29 22:19:04 +0530

    Add files via upload

---
+++ Divide and conquer/quick-sort.cpp
@@ -0,0 +1,21 @@
+==============================================
+Asked in Amazon Adobe Goldman Sachs Grofers Hike HSBC Ola Qualcomm Samsung SAP Labs Target VMWare
+==============================================
+
+int partition (int arr[], int low, int high)  
+{  
+    int pivot = arr[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            swap(&arr[i], &arr[j]);  
+        }  
+    }  
+    swap(&arr[i + 1], &arr[high]);  
+    return (i + 1);  
+}
