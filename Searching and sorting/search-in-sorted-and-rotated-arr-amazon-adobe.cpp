0ea7ac512dbc2218242a28dfbaf66a661411392d
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-01 20:47:40 +0530

    Add files via upload

---
+++ Searching and sorting/search-in-sorted-and-rotated-arr-amazon-adobe.cpp
@@ -0,0 +1,37 @@
+===============================================================
+Asked In: Adobe Amazon BankBazaar D-E-Shaw FactSet Flipkart Hike Intuit MakeMyTrip Microsoft Paytm Snapdeal Times Internet
+===============================================================
+
+int Search(vector<int> vec, int target) {
+    
+    int n = vec.size();
+    
+    int low = 0, high = n-1, ans = -1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(target == vec[mid]){
+                ans = mid;
+                break;
+            }
+
+            if(vec[low] <= vec[mid]){
+                if(vec[low] <= target && target <= vec[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(vec[mid] < vec[high]){
+                    if(vec[mid] <= target && target <= vec[high]){
+                        low = mid+1;
+                    }
+                    else{
+                        high = mid-1;
+                    }
+                }
+            }
+        }
+        return ans;
+}
