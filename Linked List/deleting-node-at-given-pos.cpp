5a303122a396ac95e0dd5d82ea2424161ad6db51
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-16 20:41:15 +0530

    Add files via upload

---
+++ Linked List/deleting-node-at-given-pos.cpp
@@ -0,0 +1,36 @@
+/* Given a reference (pointer to pointer) to the head of a list 
+   and a position, deletes the node at the given position */
+void deleteNode(struct Node **head_ref, int position) 
+{ 
+   // If linked list is empty 
+   if (*head_ref == NULL) 
+      return; 
+  
+   // Store head node 
+   struct Node* temp = *head_ref; 
+  
+    // If head needs to be removed 
+    if (position == 0) 
+    { 
+        *head_ref = temp->next;   // Change head 
+        free(temp);               // free old head 
+        return; 
+    } 
+  
+    // Find previous node of the node to be deleted 
+    for (int i=0; temp!=NULL && i<position-1; i++) 
+         temp = temp->next; 
+  
+    // If position is more than number of ndoes 
+    if (temp == NULL || temp->next == NULL) 
+         return; 
+  
+    // Node temp->next is the node to be deleted 
+    // Store pointer to the next of node to be deleted 
+    struct Node *next = temp->next->next; 
+  
+    // Unlink the node from linked list 
+    free(temp->next);  // Free memory 
+  
+    temp->next = next;  // Unlink the deleted node from list 
+} 
