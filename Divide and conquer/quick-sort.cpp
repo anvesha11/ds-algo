b48446ee87c620ebe612f31187d4175a2756ef16
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-19 16:20:22 +0530

    Update quick-sort.cpp

--- Divide and conquer/quick-sort.cpp
+++ Divide and conquer/quick-sort.cpp
@@ -19,3 +19,21 @@ int partition (int arr[], int low, int high)
     swap(&arr[i + 1], &arr[high]);  
     return (i + 1);  
 }
+/* The main function that implements QuickSort  
+arr[] --> Array to be sorted,  
+low --> Starting index,  
+high --> Ending index */
+void quickSort(int arr[], int low, int high)  
+{  
+    if (low < high)  
+    {  
+        /* pi is partitioning index, arr[p] is now  
+        at right place */
+        int pi = partition(arr, low, high);  
+  
+        // Separately sort elements before  
+        // partition and after partition  
+        quickSort(arr, low, pi - 1);  
+        quickSort(arr, pi + 1, high);  
+    }  
+}  
