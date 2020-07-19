669a36622ff881a18618ad2e2d73b8761f4cb808
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-19 14:30:31 +0530

    Add files via upload

---
+++ Linked List/reverse-a-linked-list-in-grps.cpp
@@ -0,0 +1,30 @@
+==========================================================================
+Asked In: Accolite Adobe Amazon Goldman Sachs Hike MakeMyTrip
+ Microsoft Paytm SAP Labs Snapdeal VMWare Walmart
+==========================================================================
+Node *reverse (Node *head, int k)  
+{  
+    Node* current = head;  
+    Node* next = NULL;  
+    Node* prev = NULL;  
+    int count = 0;  
+      
+    /*reverse first k nodes of the linked list */
+    while (current != NULL && count < k)  
+    {  
+        next = current->next;  
+        current->next = prev;  
+        prev = current;  
+        current = next;  
+        count++;  
+    }  
+      
+    /* next is now a pointer to (k+1)th node  
+    Recursively call for the list starting from current.  
+    And make rest of the list as next of first node */
+    if (next != NULL)  
+    head->next = reverse(next, k);  
+  
+    /* prev is new head of the input list */
+    return prev;  
+}  
