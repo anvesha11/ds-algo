33bcf005bd3c2ed69e87ff77eb7624d3816dfc00
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-19 23:16:34 +0530

    Add files via upload

---
+++ Linked List/clone-ll-with-random-pointer-and-head.cpp
@@ -0,0 +1,49 @@
+=========================================================
+Asked In: Adobe Amazon BankBazaar D-E-Shaw MakeMyTrip 
+Microsoft Morgan Stanley Ola Cabs OYO Rooms Snapdeal Walmart
+=========================================================
+
+
+
+Node *copyList(Node *head)
+{
+    if (head == NULL) return head;
+
+    Node *t = head;
+    while (t != NULL)
+    {
+        // making an identical list merged with original list
+        Node *n = new Node(t->data);
+        n->next = t->next;
+        t->next = n;
+        t = n->next;
+    }
+    
+    t = head;
+    Node *head2 = head->next;   // variable to be returned
+    while (t != NULL)
+    {
+        // setting the arbitary pointers of duplicate list
+        if (t->arb == NULL)
+            t->next->arb = NULL;
+        else 
+            t->next->arb = t->arb->next;
+        
+        t = t->next->next;
+    }
+    
+    t = head;
+    while (t != NULL)
+    {
+        // separating the 2 merged lists (duplicate and original)
+        head = t->next->next;
+        
+        if(head) t->next->next = head->next;
+        else t->next->next = NULL;
+        
+        t->next = head;
+        t = head;
+    }
+
+    return head2;
+}
