37d5174083488951f3059e046b03c4323452a4dd
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-21 19:40:20 +0530

    Add files via upload

---
+++ Hash map and set/check-if-two-arrs-are-equal.cpp
@@ -0,0 +1,17 @@
+bool check(vector<ll> arr, vector<ll> brr, int n) {
+    //code here
+    unordered_map<ll, ll> hash;
+    for(int i=0;i<n;i++)
+        hash[arr[i]]++;
+    for(int i=0;i<n;i++)
+    {
+        if(hash.find(brr[i])==hash.end())
+            return false;
+        if(hash[brr[i]]==0)
+        return false;
+        
+        hash[brr[i]]--;
+    }
+    return true;
+        
+}
