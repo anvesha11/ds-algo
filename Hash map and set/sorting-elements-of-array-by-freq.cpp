198ad06fb13602957ba3054a617c96fb5259574c
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-07-26 17:30:57 +0530

    Add files via upload

---
+++ Hash map and set/sorting-elements-of-array-by-freq.cpp
@@ -0,0 +1,29 @@
+unordered_map<int, int> m;
+
+bool sortByVal(const pair<int, int>& a, const pair<int, int>& b){
+    if(a.second == b.second) return a.first < b.first;
+    
+    return a.second > b.second;
+}
+
+
+vector<int> sortByFreq(int arr[],int n){
+    
+    vector<pair<int, int>> v;
+    
+    for(int i = 0;i<n;i++){
+        m[arr[i]]++;
+    }
+    
+    copy(m.begin(), m.end(), back_inserter(v));
+    
+    sort(v.begin(), v.end(), sortByVal);
+    vector<int> ans;
+    for(int i = 0;i<v.size();i++){
+        for(int j = 0;j<v[i].second;j++){
+            ans.push_back( v[i].first );
+        }
+    }
+    m.clear();
+    return ans;
+}
