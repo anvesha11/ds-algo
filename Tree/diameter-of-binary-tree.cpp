121fa35f4338fff7390defdcd20eebe2784ac143
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-08-20 22:19:09 +0530

    Update diameter-of-binary-tree.cpp

--- Tree/diameter-of-binary-tree.cpp
+++ Tree/diameter-of-binary-tree.cpp
@@ -32,3 +32,24 @@ int diameter(Node* p) {
     
     
 }
+
+======================================
+ Solution from geeksforgeeks
+======================================
+int go(Node* n, int* dia) {
+    if (!n) return 0;
+
+    int l = go(n->left, dia);  // height of left subtree
+    int r = go(n->right, dia); // height of right subtree
+
+    if (l + r + 1 > *dia) *dia = l + r + 1; // l+r+1 is a possible max dia
+
+    return 1 + max(l, r); // returning height of subtree
+}
+
+int diameter(Node* node) {
+    int dia = 0;
+    go(node, &dia);
+    return dia;
+}
+
