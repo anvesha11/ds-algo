047a10fc562eb182353cafe05f0ffe303bfcb477
Anvesha Shandilya <43908897+anvesha11@users.noreply.github.com>
2020-09-12 16:54:38 +0530

    Add files via upload

---
+++ Dynamic Programming/count-palindromic-subsequences.cpp
@@ -0,0 +1,17 @@
+int countPS(string str)
+{
+    int n = str.length();
+    int cps[n+1][n+1];
+    memset(cps, 0, sizeof(cps));
+    for(int i=0;i<n;i++)cps[i][i]=1;
+    for(int L=2;L<=n;L++){
+        for(int i=0;i<n;i++){
+            int k = i+L-1;
+            if(str[i]==str[k])
+                cps[i][k] = cps[i+1][k]+cps[i][k-1]+1;
+            else
+                cps[i][k] = cps[i+1][k]+cps[i][k-1]-cps[i+1][k-1];
+        }    
+    }
+    return cps[0][n-1];
+}
